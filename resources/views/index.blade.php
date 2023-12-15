c5b67b2ff743254949c1ee00eeba6ca1c9014302
José <jose.segura@unesp.br>
2023-12-15 17:23:44 -0300

    start

---
+++ resources/views/index.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <title>Barbearia</title>
+</head>
+
+<body>
+    <h1>Bem-vindo à Barbearia</h1>
+    <p>Esta é a página inicial do site da barbearia.</p>
+</body>
+
+</html>
