8ead981140f2f560891aa8beb48166845f980679
José <jose.segura@unesp.br>
2023-12-28 17:25:44 -0300

    new: first migrate and layout

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,13 +1,6 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+@include('layout.header')
 
-<head>
-    <title>Barbearia</title>
-</head>
+<h1>Bem-vindo à Barbearia</h1>
+<p>Esta é a página inicial do site da barbearia.</p>
 
-<body>
-    <h1>Bem-vindo à Barbearia</h1>
-    <p>Esta é a página inicial do site da barbearia.</p>
-</body>
-
-</html>
+@include('layout.footer')
---
+++ resources/views/layout/header.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <title>Barbearia</title>
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <!-- <script src="{{ asset('js/app.js') }}"></script> -->
+</head>
+
+<body>
